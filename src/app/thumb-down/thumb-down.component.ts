46d2605c8bb08b5ac30f498822400d5bdb98068d
Beryl01 <berylnegesa@gmail.com>
2020-04-01 17:44:09 +0300

    First Commit

---
+++ src/app/thumb-down/thumb-down.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thumb-down',
+  templateUrl: './thumb-down.component.html',
+  styleUrls: ['./thumb-down.component.css']
+})
+export class ThumbDownComponent implements OnInit {
+
+  numberoflikes : number=0
+  thumbdownbuttonclick(){
+  this.numberoflikes++;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
